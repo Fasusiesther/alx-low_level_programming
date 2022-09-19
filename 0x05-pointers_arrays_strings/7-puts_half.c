1712ff80a8df9b1e51b4964662ac6a757c427c19
Fasusiesther <fasusi4esther@yahoo.com>
2022-09-19 10:38:58 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,21 +1,33 @@
-#include "main.h"                                                                                                                       
-                                                                                                                                        
-/**                                                                                                                                     
- * puts_half - prints the second half of a string.                                                                                      
- *@str: string to use.                                                                                                                  
- */                                                                                                                                     
-                                                                                                                                        
-void puts_half(char *str)                                                                                                               
-{                                                                                                                                       
-        int length, n, i;                                                                                                               
-                                                                                                                                        
-        for (length = 0; str[length] != '\0'; length++)                                                                                 
-        {                                                                                                                               
-        }                                                                                                                               
-        n = (length - 1) / 2;                                                                                                           
-        for (i = n + 1; str[i] != '\0'; i++)                                                                                            
-        {                                                                                                                               
-                _putchar(str[i]);                                                                                                       
-        }                                                                                                                               
-        _putchar('\n');                                                                                                                 
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * followed by a new line
+ * @str: string to be printed
+ */
+void puts_half(char *str)
+{
+	int len, n, i;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	} else if (len % 2)
+	{
+		for (n = (len - 1) / 2; n < len - 1; n++)
+		{
+			_putchar(str[n + 1]);
+		}
+	}
+	_putchar('\n');
 }
