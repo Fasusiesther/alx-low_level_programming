446925b2c64ac516ae8405acc7618e14a9c4606e
Fasusiesther <fasusi4esther@yahoo.com>
2022-10-06 16:35:27 -0700

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,36 +2,43 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - A function that concatenates two strings
- * @s1: An input pointer of the first string
- * @s2: An input pointer of the second string
- * @n: an input integer of number of string to concatenate
- * Return: Appointer to concatened strings or NULL if it str is NULL
+ * string_nconcat - Concatenates two strings using at
+ *                  most an inputted number of bytes.
+ * @s1: The first string.
+ * @s2: The second string.
+ * @n: The maximum number of bytes of s2 to concatenate to s1.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - a pointer to the concatenated space in memory.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int I, j, k ;
-char *s;
-if (s1 ==NULL)
-  i=0;
-else
-{for (i=0; s1[i]; i++)
-                ;
-}
-if (s2 ==NULL)
-  i=0;
-else
-{
-for (j=0 ; s2[j]; j++)
-       ;
-}
-if (j > n)
-j = n ;
-s= malloc(sizeof(char)* (i+j +1);
-if (s==NULL)
-return(NULL);
-for (k=0; k<i ; k++)
-s[k + i] =s2[k];
-s[i + j] ='\0';
-return(s);
+	char *concat;
+	unsigned int len = n, index;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (index = 0; s1[index]; index++)
+		len++;
+
+	concat = malloc(sizeof(char) * (len + 1));
+
+	if (concat == NULL)
+		return (NULL);
+
+	len = 0;
+
+	for (index = 0; s1[index]; index++)
+		concat[len++] = s1[index];
+
+	for (index = 0; s2[index] && index < n; index++)
+		concat[len++] = s2[index];
+
+	concat[len] = '\0';
+
+	return (concat);
 }
