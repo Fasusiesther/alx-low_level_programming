f8583d5bd02e7e2b7635cc7301c4e23ff08338fa
Fasusiesther <fasusi4esther@yahoo.com>
2022-10-20 18:04:21 -0700

    First task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,9 +7,7 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
-
-	nelem = 0;
+	size_t nbr_nodes = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -17,7 +15,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		nelem++;
+		nbr_nodes++;
 	}
-	return (nelem);
+	return (nbr_nodes);
 }
