d76927cec94ebbf6a3fa3341f42e56935c503e18
Fasusiesther <fasusi4esther@yahoo.com>
2022-10-22 07:01:19 -0700

    My task 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,11 +10,10 @@ void free_list(list_t *head)
 {
 	list_t *current;
 
-	while ((current = head)  != NULL)
+	while ((current = head) != NULL)
 	{
 		head = head->next;
 		free(current->str);
 		free(current);
 	}
-
 }
