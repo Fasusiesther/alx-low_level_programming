444db57b622a633f20aee16b027ebfdc02326d35
Fasusiesther <fasusi4esther@yahoo.com>
2022-12-12 19:35:55 -0800

    Task 0

---
+++ 0x18-dynamic_libraries/mathlib.c
@@ -0,0 +1,24 @@
+int add(int a, int b);
+int sub(int a, int b);
+int mult(int a, int b);
+int divi(int a, int b);
+
+// add.c
+int add(int a, int b){
+return (a+b);
+}
+
+//sub.c
+int sub(int a, int b){
+return (a-b);
+}
+
+//mult.c
+int mult(int a, int b){
+return (a*b);
+}
+
+//divi.c
+int divi(int a, int b){
+return (a/b);
+}
